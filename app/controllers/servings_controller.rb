d8d1851acbb0b2516c95c65119538a2a7746b73d
whysthatso <git@whysthatso.net>
2022-08-22 16:01:49 +0300

    add examples of common portion size to make noting down servings easier

--- app/controllers/servings_controller.rb
+++ app/controllers/servings_controller.rb
@@ -14,6 +14,7 @@ class ServingsController < ApplicationController
   # GET /servings/new
   def new
     @serving = Serving.new
+    @common_intakes = Intake.with_common_portion
   end
 
   # GET /servings/1/edit
