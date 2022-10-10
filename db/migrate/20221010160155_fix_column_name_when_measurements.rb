70adb7364cb026fca2fa15175ad896c0b15ca627
whysthatso <git@whysthatso.net>
2022-10-11 00:55:07 +0300

    fix migration name

--- db/migrate/20221010160155_fix_column_name_when_measurements.rb
+++ db/migrate/20221010160155_fix_column_name_when_measurements.rb
@@ -1,4 +1,4 @@
-class FixColumnNameWhen < ActiveRecord::Migration[7.0]
+class FixColumnNameWhenMeasurements < ActiveRecord::Migration[7.0]
   def change
     rename_column :measurements, :when, :measured_at
   end
