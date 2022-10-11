5a8879dff03c589b69a36997e5a526dba7adbb68
whysthatso <git@whysthatso.net>
2022-10-12 01:23:50 +0300

    bugfix

--- app/helpers/measurements_helper.rb
+++ app/helpers/measurements_helper.rb
@@ -1,5 +1,5 @@
 module MeasurementsHelper
   def weight_over_time
-    Measurement.all.map {|m|  [m.consumed_at.strftime("%F"), m.weight] }
+    Measurement.all.map {|m|  [m.measured_at.strftime("%F"), m.weight] }
   end
 end
